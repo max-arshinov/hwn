a8344316cfbb66916f927f5e017062ef09384339
Max Arshinov <max@hightech.group>
2021-09-14 16:34:37 +0300

    2k-165 Initial

---
+++ src/Homework1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Homework1
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var val1 = int.Parse(args[0]);
+            var operation = args[1];
+            var val2 = int.Parse(args[2]);
+            var result = 0;
+            
+            switch (operation)
+            {
+                case "+":
+                    result = val1 + val2; break;
+                case "-":
+                    result = val1 - val2;
+                    break;
+                case "*": 
+                    result = val1 * val2;
+                    break;
+                case "/": 
+                    result = val1 / val2;
+                    break;
+            }
+            
+            Console.WriteLine($"{args[0]}{args[1]}{args[2]}={result}");
+        }
+    }
+}
